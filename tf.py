0f25bc1703447e3afe57c85bf1ca004388fa9f75
iev6 <giridhursriram@gmail.com>
2016-11-06 16:00:53 +0530

    Tried 3 techniques : 1. ELU + RMSPropOptimizer(47, stuck in local optima) 2. ELU + Adam (Momentum+RMSProp) (51)

--- tf.py
+++ tf.py
@@ -1,7 +1,7 @@
 '''
 Sample script for running tensorflow
 @Author : Giridhur S
-source : https://www.tensorflow.org/versions/r0.11/tutorials/mnist/beginners/index.html\
+based on : https://www.tensorflow.org/versions/r0.11/tutorials/mnist/beginners/index.html\
 '''
 import tensorflow as tf
 import cPickle as pkl
@@ -14,15 +14,19 @@ RANDOM_SEED = 42
 tf.set_random_seed(RANDOM_SEED)
 
 def init_weights(shape):
-    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+    #implement Xavier initialization
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.01))
 
-def model(X, w_h, w_h2, w_o, p_keep_input, p_keep_hidden): # this network is the same as the previous one except with an extra hidden layer + dropout
+
+def model(X, w_h, w_h2,w_h3, w_o, p_keep_input, p_keep_hidden): # this network is the same as the previous one except with an extra hidden layer + dropout
     X = tf.nn.dropout(X, p_keep_input)
-    h = tf.nn.sigmoid(tf.matmul(X, w_h))
+    h = tf.nn.relu(tf.matmul(X, w_h))
     h = tf.nn.dropout(h, p_keep_hidden)
-    h2 = tf.nn.sigmoid(tf.matmul(h, w_h2))
+    h2 = tf.nn.relu(tf.matmul(h, w_h2))
     h2 = tf.nn.dropout(h2, p_keep_hidden)
-    return tf.matmul(h2, w_o)
+    h3 = tf.nn.relu(tf.matmul(h2,w_h3))
+    h3 = tf.nn.dropout(h3,p_keep_hidden)
+    return tf.matmul(h3, w_o)
 
 
 ''' Splitting data'''
@@ -34,16 +38,16 @@ no_of_classes = 12
 
 pca = PCA(n_components=200)
 trainf1 = pca.fit_transform(trainf[:,:-1]) #in memory computation
+trainf1 = trainf
 
-
-N = trainf1.shape[0]
-M = trainf1.shape[1] #last col was label
+N = trainf.shape[0]
+M = trainf.shape[1] #last col was label
 
 labels_OH  = np.zeros([N,no_of_classes])
 labels_OH[np.arange(N),labels] = 1
 
 #adding a bias column
-train = np.ones([N,M+1])
+train = np.ones([N,M])
 train[:,1:] = trainf[:,:-1] #preprending the column of ones
 
 #NOTE for using sparse_softmax_cross_entropy_with_logits, input should be labels and not labels_OH
@@ -60,37 +64,46 @@ y_ = tf.placeholder(tf.float32,shape=[None,y_size]) #one hot prediction
 #y_ = tf.placeholder(tf.int32,shape=[None]) #label
 
 #init weights of layers, w_h1 = hidden1, w_h2 = hidden2 and w_o = output
-w_h1_size = 100
-w_h2_size = 50
+w_h1_size = 300
+w_h2_size = 100
+w_h3_size = 12
 w_h1 = init_weights([x_size, w_h1_size])
 w_h2 = init_weights([w_h1_size, w_h2_size])
-w_o = init_weights([w_h2_size, y_size])
+w_h3 = init_weights([w_h2_size,w_h3_size])
+w_o = init_weights([w_h3_size, y_size])
 
 #dropout params NOTE : DROPOUT WILL NOT BE PERFORMED ON THE FINAL OUTPUT LAYER!
 p_keep_input = tf.placeholder(tf.float32)
 p_keep_hidden = tf.placeholder(tf.float32)
 
-py_x = model(x, w_h1, w_h2, w_o, p_keep_input, p_keep_hidden)
+#learning_rate
+global_step = tf.Variable(0, trainable=False)
+lr = tf.train.exponential_decay(0.01,global_step=global_step,decay_rate=0.9,decay_steps=250,staircase=True)
+py_x = model(x, w_h1, w_h2,w_h3, w_o, p_keep_input, p_keep_hidden)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x,y_))
 #cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(py_x,y_))
-train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
-#train_op = tf.train.AdadeltaOptimizer(learning_rate=0.001, rho=0.95, epsilon=1e-08).minimize(cost)
+#train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
+train_op = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-5,use_locking=False, name='Adam').minimize(cost,global_step=global_step)
 predict_op = tf.argmax(py_x, 1)
 
 #k=0 #when using sparse_softmax_cross_entropy_with_logits
-k=1  when running one hot label
-MAX_ITER = 500
+k=1  #when running one hot label
+MAX_ITER = 801
 
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
 start_time = dt.datetime.now()
 with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
     # you need to initialize all variables
     tf.initialize_all_variables().run()
+    # restoring from previous checkpoint (keep in mind that layers shouldnt change)
+    # restorer = tf.train.Saver()
+    # iter = 400
+    # restorer.restore(sess,'checkpoint_'+str(iter)+'.chk')
     for i in xrange(MAX_ITER):
-        for start, end in zip(range(0, len(trX), 256), range(256, len(trX)+1, 256)):
+        for start, end in zip(range(0, len(trX), 128), range(128, len(trX)+1, 128)):
             sess.run(train_op, feed_dict={x: trX[start:end], y_: trY[start:end],
-                                          p_keep_input: 1.0, p_keep_hidden: 0.8})
-        if i%500==0:
+                                          p_keep_input: 0.8, p_keep_hidden: 0.6})
+        if i%100==0:
             saver = tf.train.Saver([w_h1,w_h2,w_o])
             saver.save(sess, 'checkpoint_'+str(i)+'.chk')
         #for prediction dropout rate should be set to 0, ie keep rate to 1
@@ -100,4 +113,4 @@ with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
                                                          p_keep_input: 1.0,
                                                          p_keep_hidden: 1.0})))
         print(i,np.mean(np.argmax(trY,axis=k)==sess.run(predict_op,feed_dict={x:trX,y_:trY,p_keep_input:1.0,p_keep_hidden:1.0})))
-print "\n time taken "+str(dt.now()-start_time)
+print "\n time taken "+str(dt.datetime.now()-start_time)
